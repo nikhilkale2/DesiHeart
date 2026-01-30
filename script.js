88b0d5fda358d319cee58d929c3bae2d667587b5
Nikhilkale2 <nikhilkaledev@gmail.com>
2026-01-30 19:12:46 +0530

    apply GSAP animation on about & Menu item

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let mm = gsap.matchMedia();
+
 // Initialize Lenis
 const lenis = new Lenis();
 function lenisScrolling() {
@@ -10,6 +12,91 @@ function lenisScrolling() {
   requestAnimationFrame(raf);
 }
 lenisScrolling();
+
+// GSAP Animation
+
+gsap.from("#website-logo", {
+  y: -60,
+  duration: 0.8,
+  opacity: 0,
+});
+function smallScreenAnimation() {
+  mm.add("(max-width:1024px)", () => {});
+}
+smallScreenAnimation();
+
+function bigScreenAnimation() {
+  mm.add("(min-width:1400px)", () => {
+    // For Hero section
+    let hero = gsap.timeline();
+    hero.from(
+      "#Hero-box",
+      {
+        x: -100,
+        opacity: 0,
+        duration: 0.8,
+      },
+      "hero",
+    );
+    hero.from(
+      "#Hero-img",
+      {
+        x: 100,
+        opacity: 0,
+        duration: 0.8,
+      },
+      "hero",
+    );
+
+    // For About
+    let about = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#About",
+        scroller: "body",
+        start: "top 55%",
+        end: "top 20%",
+        scrub: true,
+      },
+    });
+    about.from(
+      ".about-page",
+      {
+        x: -80,
+        opacity: 0,
+        duration: 0.8,
+      },
+      "about",
+    );
+    about.from(
+      ".about-img",
+      {
+        x: 80,
+        opacity: 0,
+        duration: 0.7,
+      },
+      "about",
+    );
+
+    // For Menu
+    let menu = gsap.timeline();
+    menu.from("#menu-item", {
+      y: -40,
+      opacity: 0,
+      duration: 0.8,
+      stagger: 0.3,
+      scrollTrigger: {
+        trigger: "#Menu",
+        scroller: "body",
+        start: "top 55%",
+        end: "top 25%",
+        scrub: true,
+      },
+    });
+
+    // For menu Dishes
+  });
+}
+bigScreenAnimation();
 
 let NavMenu = document.querySelector("#NavMenu");
 let NavLink = document.querySelector("#nav-dialogue");
